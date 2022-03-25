85c0f255c9749e3c65c4c5365273c2e0c0297bdb
son phan <sonphan18071999@gmail.com>
2022-03-25 17:14:51 +0700

    Allow Cors

--- server.js
+++ server.js
@@ -1,74 +1,80 @@
-const express = require('express');
+const express = require("express");
 // set up express app
 const app = express();
-var http = require('http'); //the variable doesn't necessarily have to be named http
-const dotenv = require('dotenv')
+var http = require("http"); //the variable doesn't necessarily have to be named http
+const dotenv = require("dotenv");
 
 // set up port
-const port = process.env.PORT || 4000 ;
-const bodyParser=require('body-parser');
-const mongoose=require('mongoose');
-const start = require('./app/routes/mainRoute'); 
+const port = process.env.PORT || 4000;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const start = require("./app/routes/mainRoute");
 const fileupload = require("express-fileupload");
-var cors = require('cors')
-var db = require('./app/models/mainModels')
+var cors = require("cors");
+var db = require("./app/models/mainModels");
 // set up dependencies
 app.use(cors());
-var server = http.createServer((req, res) => {
-});
-dotenv.config({ path: './database.env' })
+var server = http.createServer((req, res) => {});
+dotenv.config({ path: "./database.env" });
 
 server = app.listen(port);
-var io = require('socket.io')(server, {
+var io = require("socket.io")(server, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
-    allowedHeaders: ["Access-Control-Allow-Origin","Access-Control-Allow-Origin"],
-    credentials: true
-  }
+    allowedHeaders: ["Access-Control-Allow-Origin"],
+    credentials: false,
+  },
 });
 
-app.use(fileupload({
-  useTempFiles:true
-}));
-
+app.use(
+  fileupload({
+    useTempFiles: true,
+  })
+);
 
 //Un limit request
-app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.json({ limit: "50mb" }));
 // app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:52428800}));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 52428800,
+  })
+);
 /**Mongo Db */
 // set up mongoose
 
-
-mongoose.connect(process.env.DB_URL, { 
-  useNewUrlParser: true,
-  useFindAndModify:false,
-  useCreateIndex:true,
-  useUnifiedTopology: true})
-  .then(()=> {
-    console.log('Database connected');
-    /**Configure socket.io */
-    io.on('connection', (socket) => {
-      socket.on('disconnect', () => {
-        console.log('user disconnected');
-      });
-      socket.on('broadcast', (msg) => {
-        socket.broadcast.emit("update state comment", msg);
-      })
-    });
-  /**Configure socket.io */
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
   })
-  .catch((error)=> {
-    console.log('Error connecting to database'+error);
+  .then(() => {
+    console.log("Database connected");
+    /**Configure socket.io */
+    io.on("connection", (socket) => {
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+      });
+      socket.on("broadcast", (msg) => {
+        socket.broadcast.emit("update state comment", msg);
+      });
+    });
+    /**Configure socket.io */
+  })
+  .catch((error) => {
+    console.log("Error connecting to database" + error);
   });
 
-  
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
 //Use api
 
-app.use('/api',start);
+app.use("/api", start);
